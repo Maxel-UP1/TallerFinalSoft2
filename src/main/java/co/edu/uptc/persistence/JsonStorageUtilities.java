0387f9b00a164f01c6d32361355b51d1e4e4db33
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2025-11-19 12:32:08 -0500

    Persiencia de Gson google

---
+++ src/main/java/co/edu/uptc/persistence/JsonStorageUtilities.java
@@ -0,0 +1,59 @@
+package co.edu.uptc.persistence;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonStorageUtilities {
+    private Gson gson;
+
+    private static final String FILEPATH = "src/main/java/persistence/";
+    private static final String EXTENSION = ".json";
+
+    public JsonStorageUtilities(){
+        //El gson esta inicializado asi para que se escriba en cascada y no en una misma linea
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        //Esto es para que reconozca siempre el contenido del archivo, ya que si no lo hacemos asi
+        //cuando se agregue algo nuevo se va a sobreescribir
+        //readContentFromFile();
+        //readPersons("people");
+        ///readAccounts("accounts");
+    }
+
+    public <T> void saveDataToFile(List<T> dataList, String fileName, Type type) {
+        File file = new File(FILEPATH + fileName + EXTENSION);
+        if (dataList == null) {
+            dataList = new ArrayList<>();
+        }
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            gson.toJson(dataList, type, fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <T> List<T> readContentFromFile(String fileName, Type type) {
+        List<T> dataList = new ArrayList<>();
+
+        File file = new File( FILEPATH + fileName + EXTENSION);
+        if (!file.exists()) {
+            System.out.println("hola");
+            return null;
+        }
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            dataList.clear();
+            dataList = gson.fromJson(bufferedReader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return dataList;
+    }
+
+
+
+}
